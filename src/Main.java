05e9da38230ac046314e17e8f1d866c6e72f6223
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-06-27 21:50:55 +0300

    examples

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        // Задание
+
+        // Дано:
+        String str = "dsf sdfasd asdfadfd asdfasd asdfasdf ";
+
+        // Результат:
+        // d -> 5
+        // s -> 7
+        // a -> 1
+
+        Map<Character, Integer> charsCount = new HashMap<>();
+
+        for (int i=0; i<str.length(); i++) {
+            Character ch = Character.toUpperCase(str.charAt(i));
+            if(ch.equals(' ')) continue;
+
+            if(charsCount.containsKey(ch)) {
+                Integer count = charsCount.get(ch);
+                count++;
+                charsCount.put(ch, count);
+            } else {
+                charsCount.put(ch, 1);
+            }
+        }
+
+        System.out.println(charsCount);
+
+        // ==========================================
+
+        String text = "sdfsdf sdfsdfsd sdfsdfs sdfsdfsdf sdfsdf";
+        String[] words = text.split(" ");
+        System.out.println("words = " + Arrays.toString(words));
+
+
+        // ==========================================
         Map<String, UserProfile> users = new HashMap<>();
         users.put("sergei", new UserProfile("Sergei", "sergei@mail.com"));
         users.put("viktor", new UserProfile("Viktor", "viktor@mail.com"));
